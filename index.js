bedc713e34de4195dcaca71478e448b3378cd701
Artur <info@aigroupusa.com>
2021-12-24 16:11:40 -0500

    commit tree 4a900ef09641cf1664465fc061bbb66286a47a52

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array) {
+    const drivers = array.slice(0, 2)
+    return drivers;
+};
+
+function returnLastTwoDrivers(array) {
+    const drivers = array.slice(-2)
+    return drivers;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(arg) {
+  return  function(fare) {return arg * fare};
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, fn) {
+    return fn(drivers);
+}
+
